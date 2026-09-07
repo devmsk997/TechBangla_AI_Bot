249e91ec548b6ecd136c60c59a1389b797549a2e
Md. Solayman Kabir <solaymankabir997@gmail.com>
2026-09-07 21:40:32 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-# GitHub Secrets / Environment Variables থেকে মান সংগ্রহ
+# GitHub Secrets / Environment Variables
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 BLOG_ID = os.environ.get("BLOG_ID")
 CLIENT_ID = os.environ.get("CLIENT_ID")
@@ -8,4 +8,4 @@ CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 GITHUB_TOKEN = os.environ.get("GH_TOKEN") or os.environ.get("GITHUB_TOKEN")
 
 GITHUB_USERNAME = "devmsk997"
-GITHUB_REPO = "techbangla-image-hosting" # আপনার ইমেজ রেপোজিটরি নাম
+GITHUB_REPO = "techbangla-image-hosting"
